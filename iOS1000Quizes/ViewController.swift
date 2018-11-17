539ec79b50fbecbafafafd19bdbe70f079831d52
mac <mac@justin.lan>
2018-11-17 10:10:48 +0800

    Initial Commit

---
+++ iOS1000Quizes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS1000Quizes
+//
+//  Created by mac on 2018/11/17.
+//  Copyright © 2018年 FYZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
